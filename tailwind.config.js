54310a873ce198bb26f88b06ff9c2681d244bc22
Montee <montee.alm@gmail.com>
2025-11-15 18:23:05 +0000

    Updates to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./public/**/*.html", // Points to your HTML file
+    "./public/**/*.js",   // Points to your main JS file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
